1d52b741315c82cd9a538f9f685f74d2a42fc40c
TelesNascimento25 <Darlyssonteles98@outlook.com>
2023-06-09 17:22:06 -0300

    commit 1

---
+++ aplixproject2/android/app/src/main/kotlin/com/aplixadvisor2/aplixapp2/aplixproject2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aplixadvisor2.aplixapp2.aplixproject2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ aplixproject2/android/app/src/main/kotlin/com/aplixadvisory/aplixapp2/aplixproject2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.aplixadvisory.aplixapp2.aplixproject2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
